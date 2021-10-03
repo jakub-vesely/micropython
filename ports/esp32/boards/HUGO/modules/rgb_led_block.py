5afdcde71bc3190a2f0b2296d2408f4cf87e6b14
Jakub Vesely <jakub_vesely@seznam.cz>
2021-10-03 10:10:42 +0200

    rgb_block renamed

--- ports/esp32/boards/HUGO/modules/rgb_led_block.py
+++ ports/esp32/boards/HUGO/modules/rgb_led_block.py
@@ -1,13 +1,13 @@
 from block_base import BlockBase
 from active_variable import ActiveVariable
 
-class RgbBlockCommand():
+class RgbLedBlockCommand():
     set_rgb = 1 # followed by 3 bytes (RGB)
     set_color = 2 # foloowd by the color code
     set_on = 3
     set_off = 4
 
-class RgbBlockColor():
+class RgbLedBlockColor():
   black = 1
   white = 2
   red = 3
@@ -32,27 +32,27 @@ class RgbBlock(BlockBase):
 
   def toggle(self):
     if self.state:
-      self.tiny_setter(RgbBlockCommand.set_off)
+      self.tiny_write(RgbLedBlockCommand.set_off)
     else:
-      self.tiny_setter(RgbBlockCommand.set_on)
+      self.tiny_write(RgbLedBlockCommand.set_on)
     self.state.set_value(not self.state)
 
   def set_rgb(self, red: int, green: int, blue: int):
-    self.tiny_setter(
-        RgbBlockCommand.set_rgb,
+    self.tiny_write(
+        RgbLedBlockCommand.set_rgb,
         red.to_bytes(1, 'big') + green.to_bytes(1, 'big') + blue.to_bytes(1, 'big')
     )
     self.state.set_value(red or green or blue)
 
   def set_color(self, color: int):
     print("set_color")
-    self.tiny_setter(RgbBlockCommand.set_color, color.to_bytes(1, 'big'))
+    self.tiny_write(RgbLedBlockCommand.set_color, color.to_bytes(1, 'big'))
     self.state.set_value(color != RgbBlockColor.black)
 
   def set_on(self):
-    self.tiny_setter(RgbBlockCommand.set_on)
+    self.tiny_write(RgbLedBlockCommand.set_on)
     self.state.set_value(True)
 
   def set_off(self):
-    self.tiny_setter(RgbBlockCommand.set_off)
+    self.tiny_write(RgbLedBlockCommand.set_off)
     self.state.set_value(False)
