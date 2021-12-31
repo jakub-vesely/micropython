db4da32b1698d843423945476c7fc7f11562faad
Jakub Vesely <jakub_vesely@seznam.cz>
2021-12-31 13:55:30 +0100

    mem_info

--- ports/esp32/boards/HUGO/modules/main_block.py
+++ ports/esp32/boards/HUGO/modules/main_block.py
@@ -1,4 +1,5 @@
 import machine
+import gc
 import planner
 from ble import Ble
 
@@ -11,6 +12,13 @@ def reboot():
   print("rebooting")
   ble.disconnect()
   planner.postpone(0.1, _reboot)
+
+def get_mem_info():
+    free = gc.mem_free()
+    allocated = gc.mem_alloc()
+    total = free + allocated
+    percent = "{0:.2f} %".format(free /  total * 100)
+    return "Free mem: {0} ({1})".format(free, percent)
 
 def run():
   ble.get_shell().load_events()
