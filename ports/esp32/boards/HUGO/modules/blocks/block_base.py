a46baf0d7f43312e7ab156a2b89ff2b6a35c3f72
Jakub Vesely <jakub_vesely@seznam.cz>
2023-09-07 07:52:23 +0200

    undefined expected length changed from 0 to -1

--- ports/esp32/boards/HUGO/modules/blocks/block_base.py
+++ ports/esp32/boards/HUGO/modules/blocks/block_base.py
@@ -75,23 +75,24 @@ class BlockBase:
       return
     self.__tiny_write_common(self.type_id, command, data, silent)
 
-  def __tiny_read_common(self, type_id: int, command: int, in_data: typing.Union[bytes,None]=None, expected_length: int=0, silent=False):
+  def __tiny_read_common(self, type_id: int, command: int, in_data: typing.Union[bytes,None]=None, expected_length: int=-1, silent=False):
     """
     reads data form tiny_block via I2C
     @param type_id: block type id
     @param command: one byte command
     @param in_data: specify input data for entered command
-    @param expected_length: if defined will be read entered number of bytes. If None is expected length as a first byte
+    @param expected_length: if defined (> 0)will be read entered number of bytes. If None is expected length as a first byte
     @return provided bytes. If size is first byte is not included to output data
     """
     if self._check_type(type_id):
       self._raw_tiny_write(type_id, command, in_data, silent)
       try:
-        if expected_length == 0:
+        if expected_length > 0:
+          data = self.i2c.readfrom(self.address, expected_length, True)
+        else:
           data = self.i2c.readfrom(self.address, 255, True)
           data = data[1: data[0]+1]
-        else:
-          data = self.i2c.readfrom(self.address, expected_length, True)
+
         return data
       except OSError:
         if not silent:
@@ -101,10 +102,10 @@ class BlockBase:
       self.logging.error("invalid block type - reading interupted")
       return None
 
-  def _tiny_read_base_id(self, command: int, in_data: typing.Union[bytes,None]=None, expected_length: int=0, silent=False):
+  def _tiny_read_base_id(self, command: int, in_data: typing.Union[bytes,None]=None, expected_length: int=-1, silent=False):
     return self.__tiny_read_common(_i2c_block_type_id_base, command, in_data, expected_length, silent)
 
-  def _tiny_read(self, command: int, in_data: typing.Union[bytes,None]=None, expected_length: int=0):
+  def _tiny_read(self, command: int, in_data: typing.Union[bytes,None]=None, expected_length: int=-1):
     if not self.is_available():
       return None
 
