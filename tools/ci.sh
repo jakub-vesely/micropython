jakub-vesely/micropython

commit 1a2dfc115ac87ba8ac86d120d568c8b6df0a6f3f

tools/ci.sh: Exit with error if zcat fails during ci_esp8266_setup.
Signed-off-by: Jeff Epler <jepler@unpythonic.net>

tools/ci.sh
@@ -232,7 +232,7 @@ function ci_esp32_build_c2_c6 {
 function ci_esp8266_setup {
     sudo pip3 install pyserial esptool==3.3.1 pyelftools ar
     wget https://micropython.org/resources/xtensa-lx106-elf-standalone.tar.gz
-    zcat xtensa-lx106-elf-standalone.tar.gz | tar x
+    (set -o pipefile; zcat xtensa-lx106-elf-standalone.tar.gz | tar x)
     # Remove this esptool.py so pip version is used instead
     rm xtensa-lx106-elf/bin/esptool.py
 }

commit faabe9715ee5daa9fd9780af8844758b0633924f

tools/ci: Fix typo of pipefail.
Regression from 1a2dfc115ac87ba8ac86d120d568c8b6df0a6f3f.

Signed-off-by: Angus Gratton <angus@redyak.com.au>

tools/ci.sh
@@ -241,7 +241,7 @@ function ci_esp32_build_c2_c6 {
 function ci_esp8266_setup {
     sudo pip3 install pyserial esptool==3.3.1 pyelftools ar
     wget https://micropython.org/resources/xtensa-lx106-elf-standalone.tar.gz
-    (set -o pipefile; zcat xtensa-lx106-elf-standalone.tar.gz | tar x)
+    (set -o pipefail; zcat xtensa-lx106-elf-standalone.tar.gz | tar x)
     # Remove this esptool.py so pip version is used instead
     rm xtensa-lx106-elf/bin/esptool.py
 }